09df2f38a97770312f9154066af82b80c9f438e4
ganeshpodishetti <ganeshpodishetti@gamil.com>
2021-05-22 22:07:47 +0530

    adding new files

---
+++ CSharp_Fundamentals/18. Time Span/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _18._Time_Span
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // creating time span
+            var timeSpan = new TimeSpan(1, 2, 3);
+            Console.WriteLine(timeSpan);
+
+            var timeSpan2 = new TimeSpan(1, 0, 0);
+            var timeSpan3 = TimeSpan.FromHours(1);
+            Console.WriteLine(timeSpan3);
+
+            var start = DateTime.Now;
+            var end = DateTime.Now.AddMinutes(2);
+            var duration = end - start;
+            Console.WriteLine("Duration : " + duration);
+
+            // properties
+            Console.WriteLine("minutes : " + timeSpan.Minutes);
+            Console.WriteLine("Total minutes : " + timeSpan.TotalMinutes);
+
+            // Add
+            Console.WriteLine("Add example : " + timeSpan.Add(TimeSpan.FromMinutes(8)));
+            Console.WriteLine("Substract : " + timeSpan.Subtract(TimeSpan.FromMinutes(2)));
+
+            // ToString
+            Console.WriteLine("To string : " + timeSpan.ToString());
+
+            // Parse
+            Console.WriteLine("Parse : " + TimeSpan.Parse("01:03:02"));
+        }
+    }
+}
---
+++ CSharp_Fundamentals/19. String/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _19._String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fullName = "Ganesh Podishetti";
+            Console.WriteLine("Trim : " + fullName.Trim());
+            Console.WriteLine("ToUpper : " + fullName.Trim().ToUpper());
+
+            var index = fullName.IndexOf(' ');
+            var firstName = fullName.Substring(0, index);
+            var lastName = fullName.Substring(index + 1);
+            Console.WriteLine("first name : " + firstName);
+            Console.WriteLine("last name : " + lastName);
+
+            var name = fullName.Split(' ');
+            Console.WriteLine("first name : " + name[0]);
+            Console.WriteLine("last name : " + name[1]);
+
+            Console.WriteLine(fullName.Replace("Ganesh", "Sandhya"));
+
+            if (String.IsNullOrWhiteSpace(" "))
+                Console.WriteLine("Invalid");
+
+            var str = "25";
+            var age = Convert.ToByte(str);
+            Console.WriteLine(age);
+
+            float p = 29.01f;
+            var a = p.ToString("C0");
+            Console.WriteLine(a);
+        }
+    }
+}
